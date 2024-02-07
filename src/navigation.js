f2f74f44fd97163a69eba2244edc418f6fa8f889
Viacheslav Onatskiy <slava9678@gmail.com>
2024-02-06 21:28:59 -0800

    Navigation logic

--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,7 @@
+import { FontAwesome5 } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Pressable, Text } from "react-native";
 import ProductDetailsScreen from "./screens/ProductDetailsScreen";
 import ProductsScreen from "./screens/ProductsScreen";
 import ShoppingCart from "./screens/ShoppingCart";
@@ -9,8 +11,24 @@ const Stack = createNativeStackNavigator();
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Products" component={ProductsScreen} />
+      <Stack.Navigator
+        screenOptions={{ contentStyle: { backgroundColor: "white" } }}
+      >
+        <Stack.Screen
+          name="Products"
+          component={ProductsScreen}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <Pressable
+                onPress={() => navigation.navigate("Cart")}
+                style={{ flexDirection: "row" }}
+              >
+                <FontAwesome5 name="shopping-cart" size={18} color="gray" />
+                <Text style={{ marginLeft: 5, fontWeight: "500" }}>1</Text>
+              </Pressable>
+            ),
+          })}
+        />
         <Stack.Screen
           name="Product Details"
           component={ProductDetailsScreen}
