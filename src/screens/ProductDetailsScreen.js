87aa24ae22488ebd637fc518524dfe0a404188c2
Viacheslav Onatskiy <slava9678@gmail.com>
2024-02-06 22:21:33 -0800

    Setup Redux toolkit and implement Product Slice

--- src/screens/ProductDetailsScreen.js
+++ src/screens/ProductDetailsScreen.js
@@ -8,10 +8,10 @@ import {
   View,
   useWindowDimensions,
 } from "react-native";
-import products from "../data/products";
+import { useSelector } from "react-redux";
 
 const ProductDetailsScreen = () => {
-  const product = products[0];
+  const product = useSelector((state) => state.products.selectedProduct);
 
   const { width } = useWindowDimensions();
 
--- src/screens/ProductsScreen.js
+++ src/screens/ProductsScreen.js
@@ -1,15 +1,23 @@
 import { FlatList, Image, Pressable, StyleSheet } from "react-native";
-import products from "../data/products";
+import { useDispatch, useSelector } from "react-redux";
+import { productsSlice } from "../store/productsSlice";
 
 const ProductsScreen = ({ navigation }) => {
   // const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  const products = useSelector((state) => state.products.products);
 
   return (
     <FlatList
       data={products}
       renderItem={({ item }) => (
         <Pressable
-          onPress={() => navigation.navigate("Product Details")}
+          onPress={() => {
+            dispatch(productsSlice.actions.setSelectedProduct(item.id));
+
+            navigation.navigate("Product Details");
+          }}
           style={styles.itemContainer}
         >
           <Image source={{ uri: item.image }} style={styles.image} />
