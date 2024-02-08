9f9f04d6ac32a509b77ee4607be40bc3ea60fa94
Viacheslav Onatskiy <slava9678@gmail.com>
2024-02-07 19:00:51 -0800

    Edit Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # rn-NikeApp
+
 React Native Nike App
+
+---
+
+## react-navigation, redux
+
+---
+
+![Preview image 1]('assets/preview_1.jpg')
+![Preview image 2]('assets/preview_2.jpg')
+![Preview image 3]('assets/preview_3.jpg')
--- src/data/cart.js
+++ src/data/cart.js
@@ -1,10 +1,10 @@
 export default [
   {
     product: {
-      id: "1",
+      id: '1',
       image:
-        "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png",
-      name: "Wild Berry",
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png',
+      name: 'Wild Berry',
       price: 160,
     },
     size: 42,
@@ -12,10 +12,10 @@ export default [
   },
   {
     product: {
-      id: "2",
+      id: '2',
       image:
-        "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike2.png",
-      name: "Air Force 1",
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike2.png',
+      name: 'Air Force 1',
       price: 169,
     },
     size: 43,
@@ -23,10 +23,10 @@ export default [
   },
   {
     product: {
-      id: "3",
+      id: '3',
       image:
-        "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike3.png",
-      name: "Nike Cosmic",
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike3.png',
+      name: 'Nike Cosmic',
       price: 129,
     },
     size: 44,
